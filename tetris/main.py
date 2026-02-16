d8f4e1143bb298c5c63b8ec6f63a935f73ac3f26
Bedboom7217 <ethan.m.he@outlook.com>
2026-02-16 12:00:13 -0700

    Add Tetris Game.

--- tetris/main.py
+++ tetris/main.py
@@ -6,15 +6,33 @@ from enum import Enum
 
 # Initialize Pygame
 pygame.init()
+pygame.mixer.init()
 
 # Constants
 WINDOW = (800, 800)
 BLOCK = 40
 START_SPEED = 1
+HARD_MODE_SPEED = 2
 SPEED_INCREASE = 0.5
+HARD_MODE_SPEED_INCREASE = 1
 FPS = 30
 LEVEL_UP_SCORE = 10
 LEVEL_29_SPEED = 15 #Symbolic NES level
+HARD_LEVEL_29_SPEED = 30 #Double the speed for hard mode level 29
+VICTORY_POEM = '''TETRIS
+The game of falling blocks, a test of skill and reflexes
+With each piece that falls, a new challenge arises
+The player must think quickly, plan ahead, and adapt to the ever-changing board
+Lines are cleared, points are scored, and the game speeds up
+You are a hero. A master of Tetris. You have conquered the 29th level, the legendary final stage
+But the game is never truly over. The challenge continues, as you strive for higher scores and new records
+Congratulations, for the love of the Legend of Zelda II, you have beaten Tetris
+Yet the game may never truly end; for endless mode is coming soon.
+[TO BE CONTINUED...]
+The second last line is to be removed when endless mode is released, and the last line is just a teaser for it. 
+Endless mode will be a free update that adds an infinite level after level 29, with the speed increasing every 10 levels. 
+The music will also change every 10 levels, with some new tracks and some old ones from the main game.
+The goal of endless mode is to get the highest score possible before inevitably losing'''
 
 # Colors
 BGCOLOR = (0, 0, 0)
@@ -37,6 +55,7 @@ class GameState(Enum):
     PLAYING = 1
     PAUSED = 2
     GAME_OVER = 3
+    WARNING = 4 #Warning screen telling the player that the game is started in hard mode
 
 class Tetrimino():
     class Rotation(Enum):
@@ -204,6 +223,8 @@ class TetrisGame:
         self.triples = 0
         self.doubles = 0
         self.last_move_time = time.time()
+        self.hard_mode = False
+        self.endless_mode = False
         global blocks
         blocks.clear()
 
@@ -215,6 +236,11 @@ class TetrisGame:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     if self.state == GameState.TITLE:
+                        if self.hard_mode:
+                            self.state = GameState.WARNING
+                        else:
+                            self.state = GameState.PLAYING
+                    elif self.state == GameState.WARNING:
                         self.state = GameState.PLAYING
                     elif self.state == GameState.PLAYING:
                         self.state = GameState.PAUSED
@@ -222,6 +248,12 @@ class TetrisGame:
                         self.state = GameState.PLAYING
                     elif self.state == GameState.GAME_OVER:
                         self.__init__()
+                if event.key == pygame.K_LSHIFT and self.state == GameState.TITLE:
+                    self.endless_mode = not self.endless_mode
+                    if self.endless_mode and self.hard_mode:
+                        self.hard_mode = False #Endless mode is designed to allow players to clear levels more easily
+                    if self.endless_mode:
+                        pygame.display.set_caption("Tetris - ENDLESS MODE")
                 if self.current_tetrimino: # Cannot call methods on NoneType
                     if event.key == pygame.K_UP:
                         if self.state == GameState.PLAYING:
@@ -241,6 +273,21 @@ class TetrisGame:
                     if event.key == pygame.K_z:
                         if self.state == GameState.PLAYING:
                             self.current_tetrimino.rotate('counterclockwise')
+                else: 
+                    if event.key == pygame.K_x:
+                        if self.state == GameState.WARNING:
+                            self.state = GameState.PLAYING
+                    if event.key == pygame.K_z:
+                        if self.state == GameState.TITLE:
+                            self.hard_mode = not self.hard_mode
+                            if self.hard_mode:
+                                pygame.display.set_caption("Tetris - HARD MODE")
+                                self.speed = HARD_MODE_SPEED
+                            else:
+                                pygame.display.set_caption("Tetris")
+                                self.speed = START_SPEED
+                        if self.state == GameState.WARNING:
+                            self.state = GameState.TITLE
 
     def spawn_tetrimino(self):
         types = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
@@ -255,17 +302,28 @@ class TetrisGame:
     def play_bgm(self):
         music_folder = 'sounds'
         global prev_music_file
-        pygame.mixer.init()
         if self.state == GameState.TITLE:
             music_file = '01 Title.mp3'
-        elif self.state == GameState.PLAYING and self.level <= 10:
-            music_file = '19.mp3'
-        elif self.state == GameState.PLAYING and 11 <= self.level <= 20:
-            music_file = '4 - Deeper In the Cave....mp3' #Ominous music from Zelda II
+        elif self.state == GameState.PLAYING and not self.hard_mode:
+            if self.state == GameState.PLAYING and self.level <= 10:
+                music_file = '19.mp3'
+            elif self.state == GameState.PLAYING and 11 <= self.level <= 20:
+                music_file = '4 - Deeper In the Cave....mp3' #Ominous music from Zelda II
+        elif self.state == GameState.PLAYING and self.hard_mode:
+            music_file = 'hard_mode.mp3' #Pokemon FR/LG VS Rival music, because it's intense and fitting for hard mode
+        elif self.state == GameState.PLAYING and self.endless_mode:
+            if not self.level % 30:
+                music_file = '19.mp3' #Return to the original music every 30 levels for nostalgia
+            elif not (self.level - 10) % 30:
+                music_file = '4 - Deeper In the Cave....mp3' #Return to the ominous music every 30 levels for variety
+            elif not (self.level - 20) % 30:
+                music_file = 'hard_mode.mp3' #Return to the intense hard mode music every 30 levels for hype
         elif self.state == GameState.PLAYING and self.level == 29:
             music_file = '15 - The Palace Boss.mp3' #Epic boss music from Zelda II
         elif self.state == GameState.GAME_OVER:
             music_file = 'game_over.mp3'
+        elif self.state == GameState.WARNING:
+            music_file = 'warning.mp3' #Beeping noise to warn the player that hard mode is enabled
         else:
             return  # For PAUSED state, don't change music
         if prev_music_file == music_file:
@@ -278,13 +336,14 @@ class TetrisGame:
     def level_up(self):
         self.level += 1
         self.score = 0
-        self.speed += SPEED_INCREASE
-        if self.level == 21:
+        self.speed += SPEED_INCREASE if not self.hard_mode else HARD_MODE_SPEED_INCREASE
+        if self.level == 21 and not self.endless_mode:
             self.level = 29
-            self.speed = LEVEL_29_SPEED #Teleport to the legendary 29th level
-        if self.level == 30:
+            self.speed = HARD_LEVEL_29_SPEED if self.hard_mode else LEVEL_29_SPEED #Teleport to the legendary 29th level
+        if self.level == 30 and not self.endless_mode:
             self.state = GameState.GAME_OVER
-        blocks.clear()
+        if not self.hard_mode:
+            blocks.clear()
 
     def calculate_score(self):
         score = 0
@@ -293,6 +352,8 @@ class TetrisGame:
             score += 500 #Bonus for reaching level 29
         if self.level == 30:
             score += 1000 #Bonus for beating level 29
+        if self.level > 30:
+            score += (self.level - 30) * 100 #Bonus for each level cleared after 30 in endless mode
         score += self.tetrises * 50
         score += self.triples * 20
         score += self.doubles * 5
@@ -304,7 +365,7 @@ class TetrisGame:
         lines_cleared = 0
         for y in range(20):
             line_blocks = [block for block in blocks if block[0][1] == y]
-            if len(line_blocks) == 10:
+            if len(line_blocks) >= 10:
                 lines_cleared += 1
                 blocks = [block for block in blocks if block[0][1] != y]
                 # Drop all blocks above the cleared line
@@ -364,12 +425,24 @@ class View:
 
     def title_screen(self):
         self.screen.fill(BGCOLOR)
-        title_font = self.load_font('TetrisNew.ttf', 48)
+        if self.game.hard_mode:
+            title_font = self.load_font('HyliaSerifBeta-Regular.otf', 48)
+        else:
+            title_font = self.load_font('TetrisNew.ttf', 48)
         start_font = self.load_font('PressStart2P.ttf', 24)
         title_surface = title_font.render("TETRIS", True, BOXESCOLOR)
         start_surface = start_font.render("PUSH START", True, BOXESCOLOR)
         self.screen.blit(title_surface, (WINDOW[0] // 2 - title_surface.get_width() // 2, WINDOW[1] // 3))
         self.screen.blit(start_surface, (WINDOW[0] // 2 - start_surface.get_width() // 2, WINDOW[1] // 2))
+        pygame.display.flip()
+
+    def warning_screen(self):
+        self.screen.fill(BGCOLOR)
+        warning_font = self.load_font('PressStart2P.ttf', 24)
+        warning_surface = warning_font.render("WARNING: HARD MODE ENABLED", True, BOXESCOLOR)
+        start_surface = warning_font.render("PRESS X TO START, Z TO GO BACK", True, BOXESCOLOR)
+        self.screen.blit(start_surface, (WINDOW[0] // 2 - start_surface.get_width() // 2, WINDOW[1] // 2 + 40))
+        self.screen.blit(warning_surface, (WINDOW[0] // 2 - warning_surface.get_width() // 2, WINDOW[1] // 2))
         pygame.display.flip()
 
     def base_game_screen(self):
@@ -422,6 +495,8 @@ class View:
     def render(self):
         if self.game.state == GameState.TITLE:
             self.title_screen()
+        elif self.game.state == GameState.WARNING:
+            self.warning_screen()
         elif self.game.state in [GameState.PLAYING, GameState.PAUSED]:
             self.base_game_screen()
             self.draw_blocks()
